d7836898518122e224b44ac7b686f8d2205caf58
Germen6392 <gerbarinas25@gmail.com>
2023-02-02 18:19:14 -0300

    input agregado

--- calculadora.py
+++ calculadora.py
@@ -2,10 +2,13 @@ import ingreso_horario
 
 orden_local = ingreso_horario.asignar_tarifa_local()
 orden_cliente = ingreso_horario.asignar_tarifa_cliente()
+orden = int(input("ingresa 1 si es una sola orden, 2 si es la segunda, 3 si es la tercera: "))
+km_inicio = float(input("ingresa la distancia al local: "))
+km_fin = float(input("ingresa la distancia al cliente: "))
 km = 30
 ranking = 12
 
-def pedido(orden,km_inicio,km_fin):
+def pedido():
     if orden == 1:
         return orden_local + orden_cliente + km_inicio * km + km_fin * km + ranking * km_fin
     elif orden == 2:
@@ -15,5 +18,5 @@ def pedido(orden,km_inicio,km_fin):
     else:
         return "elige el tipo de orden según corresponda"
 
-llegada_orden = pedido(2,0.00,1.48)
+llegada_orden = pedido()
 print(f"la orden tendrá una ganancia total de $ {llegada_orden}")
